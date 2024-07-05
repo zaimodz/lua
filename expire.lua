2cbe0dfad1b617a5c68f749fc51d1dbad94f6c1b
zaimodz <156922473+zaimodz@users.noreply.github.com>
2024-07-06 00:01:52 +0800

    Update expire.lua

--- expire.lua
+++ expire.lua
@@ -1,3 +1,5 @@
-local DAY = 7
-local MONTH = 07
+local DAY = 6
+local MONTH = 7
 local YEAR = 2024
+
+local expirationTime = os.time({year = YEAR, month = MONTH, day = DAY, hour = 0, min = 0, sec = 0})
