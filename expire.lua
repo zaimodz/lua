aac588562a8458fa1caa56ab2363867166fd1149
zaimodz <156922473+zaimodz@users.noreply.github.com>
2024-07-06 14:25:46 +0800

    Update expire.lua

--- expire.lua
+++ expire.lua
@@ -2,9 +2,4 @@ local DAY = 7
 local MONTH = 7
 local YEAR = 2024
 
-local expirationTime = os.time({year = YEAR, month = MONTH, day = DAY, hour = 0, min = 0, sec = 0})
-if os.time() > expirationTime then
-  gg.alert("⚠⚠ Script expired ⚠⚠")
-  gg.setVisible(true)
-  os.exit()
-end
+
